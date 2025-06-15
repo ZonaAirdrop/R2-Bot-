d1c537c03fa9d1fad8b4d7bb8d023c936f423818
Zona Airdrop <80012026+ZonaAirdrop@users.noreply.github.com>
2025-06-16 00:33:20 +0700

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-import dotenv from 'dotenv';
-dotenv.config();
-
-import { runFaucetCommand } from './commands.js';
-
-console.clear();
-console.log("🚀 zonaairdrop Faucet is running...\n");
-
-runFaucetCommand();
